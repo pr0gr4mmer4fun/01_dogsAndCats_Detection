b2b734fa5fa5d12e6e557f4df3da3d4851d37ca3
steezystudent <112044966+steezystudent@users.noreply.github.com>
2023-05-10 18:27:39 -0700

    maintenance update
    Added: Docstrings, user input for confidence, 
    
    Removed: argparse 
    
    Still need to: fix the count feature

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import os
 from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
-import argparse
 import imutils
 import time
 import cv2
@@ -21,12 +20,18 @@ import keyboard
 
 # show versions of what packages are running to make sure that the user knows if they need to update or not
 def get_versions():
+    """Show versions of packages to inform the user about potential updates.
+
+    Returns:
+        text: Shows the current versions of the imports OpenCV, Argparse, NumPy, and Imutils.
+
+    """
+
     print('\n[INFO] running, please wait\n')
     print('\n[INFO] Versions installed:')
     print('[INFO] Getting Versions...\n')
 
     print('[INFO] cv2 version:', cv2.__version__)
-    print('[INFO] argparse version:', argparse.__version__)
     print('[INFO] numpy version:', np.__version__)
 
     # always the last one because it is the new line one
@@ -35,18 +40,38 @@ def get_versions():
 
 # get user input for how big they want the frame to be
 def get_user_input():
-    print("1. Input how big you want the frame. Ex 420x420, 720x720, 1080x1080, 1440x1440")
-    print("2. Press 'r' to run the program or 'q' to exit\n")
+    """Get user input for frame dimensions and return as a tuple.
 
-    frame_height = int(input("height: "))
-    frame_width = int(input("width: "))
-    return frame_height, frame_width
+    Inputs:
+       int: How big the user wants to make the frame size.
+
+    Returns:
+        frame: The size will depend on user preferences.
+
+    """
+
+    print("1. Input how big you want the frame. Ex 420x420, 720x720, 1080x1080, 1440x1440")
+    print("2. Press 'ENTER' twice to run the program or 'q' to exit\n")
+
+    frame_height = int(input("Height: "))
+    frame_width = int(input("Width: "))
+
+    return frame_height, frame_width,
 
 
 # The model is trained on the 21 classes below, in order to filter out...
 # unneeded information we specify which classes we want to detect
 
 def get_model_classes():
+    """Return the list of classes that the model is trained on,
+    the model must have all 21 classes present or else it cannot compare detected objects to all...
+    ..other classes making it so that it is unable to process the detected object.
+
+    Returns:
+        text: what object is being detected in the frame.
+
+    """
+
     return ["background", "aeroplane", "bicycle", "bird", "boat",
             "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
             "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
@@ -55,16 +80,34 @@ def get_model_classes():
 
 # selection process
 def select_only_what_we_want():
+    """Returns a dictionary of the classes selected for object detection along with their built-in integer."""
+
     return {"person": 15, "cat": 8, "dog": 12}
 
 
 # make it so that the bounding boxes are random colors within the RGB spectrum
 def show_colors(colors):
+    """Return random colors within the RGB spectrum for bounding boxes.
+    Param colors:
+            returns a random color for when an object is detected this color...
+    ..is applied to the bounding box.
+
+    """
+
     return np.random.uniform(0, 255, size=(len(colors), 3))
 
 
 # check if the model files are present if they are not show the error message
 def see_if_files_exist(protxtfile, model_file):
+    """Check if the model files exist and return a boolean.
+      protxtfile:
+            returns: the protxtfile that contains the images used for training.
+
+    param model_file:
+            returns: The CAFFE model file that turns the model into plain text.
+
+    """
+
     if not os.path.exists(protxtfile):
         print("[ERROR!] No protxtfile detected please check file location")
         if not os.path.exists(model_file):
@@ -77,11 +120,25 @@ def see_if_files_exist(protxtfile, model_file):
 
 # get the video and put it through the model
 def get_video(protxtfile, model_file):
+    """Load the video and return it after putting it through the model.
+
+    protxtfile:
+            returns: the protxtfile that contains the images used for training.
+
+    param model_file:
+            returns: The CAFFE model file that turns the model into plain text.
+    """
+
     return cv2.dnn.readNetFromCaffe(protxtfile, model_file)
 
 
 # start camera
 def run_camera():
+    """Initialize the camera and start the FPS counter.
+    Takes the video-feed from the users webcam and starts the timer for the fps.
+
+    """
+
     # allow the camera sensor to warm up,
     # start the FPS counter
     vs = VideoStream(src=0).start()
@@ -90,13 +147,62 @@ def run_camera():
     return vs, fps
 
 
-def Processing(vs, fps, frame_width, frame_height, net, colors, classes):
+def Processing(vs, fps, frame_width, frame_height, net, colors, classes, model_confidence):
+    """Process the video stream, display object detection, and count detected objects.
+    param: VS
+        Video-Stream takes the uses webcam and starts recording
+
+
+    Param: Fps
+        Frames per second measures how many frames per second the program is running at
+        having the fps being known is important as proper streaming should take place
+        above 30 frames per second.
+
+    Param input:
+            Returns:
+                frame_width:
+                    Frame_width is an input the user interacts with at the beginning that sets the width of the frame
+
+    Param input:
+            Returns:
+                frame_height:
+                    Frame_height is an input the user interacts with at the beginning that sets the height of the frame
+
+    Param: net
+        Using the network parameter we can now send our images for processing without creating our own Convolutional
+    Neural Network instead we can now use the built-in processing from numpy.
+
+     Param: colors
+        returns:
+            a random color for when an object is detected this color...
+        ...is applied to the bounding box. Serves an Aesthetic purpose.
+
+    Param: classes
+        Returns:
+            text: what object is being detected in the frame.
+
+    Param: model_confidence
+        input:
+            parameter:
+                Model_confidence is what float between 0.1 and 0.9 does the user want to set the confidence of the...
+                program to, the models set confidence will effect how accuracy the objects in the frame are being assigned..
+                ..to the mentioned classes.
+
+    """
+
     count = {"person": 0, "dog": 0, "cat": 0}
 
     while True:
-        # grab the frame from the threaded video stream and resize it
+        # grab the frame from the threaded video stream
         frame = vs.read()
-        frame = imutils.resize(frame, width=frame_width, height=frame_height)
+        frame_width, frame_height = int(frame_width), int(frame_height)
+
+        # Make sure dimensions are valid before resizing
+        if frame_width > 0 and frame_height > 0:
+            frame = cv2.resize(frame, (frame_width, frame_height), interpolation=cv2.INTER_AREA)
+        else:
+            print("[FRAME ERROR!] Please provide valid frame dimensions.")
+            break
 
         # grab the frame dimensions and convert it to a blob
         (h, w) = frame.shape[:2]
@@ -116,7 +222,8 @@ def Processing(vs, fps, frame_width, frame_height, net, colors, classes):
 
             # filter out weak detections by ensuring the `confidence` is
             # greater than the minimum confidence
-            if confidence > 0.2:
+
+            if confidence > model_confidence:
                 # extract the index of the class label from the
                 # `detections`
                 class_confidence = int(detections[0, 0, i, 1])
@@ -124,15 +231,16 @@ def Processing(vs, fps, frame_width, frame_height, net, colors, classes):
 
                 # PUT CLASSES HERE
                 if class_confidence in filtered_classes.values():
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    box = detections[0, 0, i, 3:9] * np.array([w, h, w, h])
                     (startX, startY, endX, endY) = box.astype("int")
-
-                    # count how many dogs humans and cats we see
-                    count[classes[class_confidence]] += 1
-
                     # draw the prediction on the frame
                     label = "{}: {:.2f}%".format(classes[class_confidence],
                                                  confidence * 100)
+                    # counting the objects
+                    class_name = classes[class_confidence]
+                    if class_name in count:
+                        count[class_name] += 1
+
                     cv2.rectangle(frame, (startX, startY), (endX, endY),
                                   colors[class_confidence].tolist(), 2)
                     y = startY - 15 if startY - 15 > 15 else startY + 15
@@ -156,21 +264,34 @@ def Processing(vs, fps, frame_width, frame_height, net, colors, classes):
     # stop the timer and display FPS information
     fps.stop()
     print("number of objects:", count)
+
     print("[INFO] elapsed time: {:.2f} seconds".format(fps.elapsed()))
-    print("[INFO] approx  FPS: {:.2f}".format(fps.fps()))
+    time_passed = fps.elapsed()
+    if time_passed > 0:
+
+        print("[INFO] FPS: {:.2f}".format(fps.fps()))
+    else:
+        print("[INFO] waiting for time to start ")
 
     # do a bit of cleanup
     cv2.destroyAllWindows()
     vs.stop()
 
 
-def display():
+def display(model_confidence):
+    """Display package versions, user input, and run object detection on the video stream.
+
+        model_confidence: object
+            Displays what settings user has chosen.
+
+    """
+
     get_versions()
 
-    frame_height, frame_width = get_user_input()
+    frame_height, frame_width, = get_user_input()
 
     while True:
-        if keyboard.is_pressed('r'):
+        if keyboard.is_pressed('enter'):
             print("\nStarting video...")
 
             protxtfile = "MobileNetSSD_deploy.prototxt.txt"
@@ -181,7 +302,7 @@ def display():
                 classes = get_model_classes()
                 colors = show_colors(classes)
                 vs, fps = run_camera()
-                Processing(vs, fps, frame_width, frame_height, net, colors, classes)
+                Processing(vs, fps, frame_width, frame_height, net, colors, classes, model_confidence)
 
         elif keyboard.is_pressed('q'):
             print('\n[INFO] Program Terminated')
@@ -189,6 +310,10 @@ def display():
 
 
 if __name__ == "__main__":
-    display()
+    print("settings \n")
+    model_confidence = float(input("Choose model accuracy (0.1 to 0.9): "))
+    if model_confidence < 0.1 or model_confidence > 0.9 or str:
+        print("Invalid choice, defaulting to value of 0.7.")
+        model_confidence = 0.5
 
-
+        display(model_confidence)
